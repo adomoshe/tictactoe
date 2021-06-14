e40f7c17eb2c5df7592faac2ee5abd8430e9693c
Ado Moshe <ado.moshe@gmail.com>
2021-06-13 23:07:28 -0700

    Working on adding socket.io connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 import Login from './components/Login';
 import Logout from './components/Logout';
+
+// eslint-disable-next-line no-unused-vars
+const socket = io('localhost:3002');
 
 export default function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,8 @@ ReactDOM.render(
       domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
       redirectUri={window.location.origin}
+      audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+      scope="read:current_user update:current_user_metadata"
     >
       <App />
     </Auth0Provider>
